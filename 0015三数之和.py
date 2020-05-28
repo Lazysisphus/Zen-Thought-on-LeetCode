9eb2954eae2f2e08567b98d10ea9cb6ec3016615
Lazysisphus <624837594@qq.com>
2020-05-28 17:55:08 +0800

    Update 0015三数之和.py

--- 0015三数之和.py
+++ 0015三数之和.py
@@ -8,11 +8,11 @@ class Solution(object):
         # https://leetcode-cn.com/problems/3sum/solution/pai-xu-shuang-zhi-zhen-zhu-xing-jie-shi-python3-by/
         n = len(nums)
         res=[]
-        if not nums or n<3:
+        if not nums or n < 3:
             return res
 
         nums.sort()
-        for i in range(n):
+        for i in range(n-2):
             # 如果当前数字已经大于0，那么后续数字没有必要遍历
             if nums[i] > 0:
                 return res
@@ -20,23 +20,23 @@ class Solution(object):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
 
-            L=i+1
-            R=n-1
-            while L < R:
-                if nums[i] + nums[L] + nums[R] == 0:
-                    res.append([nums[i], nums[L], nums[R]])
+            left = i + 1
+            right = n - 1
+            while left < right:
+                if nums[i] + nums[left] + nums[right] == 0:
+                    res.append([nums[i], nums[left], nums[right]])
 
                     # 避免重复
-                    while L<R and nums[L] == nums[L+1]:
-                        L += 1
-                    while(L<R and nums[R]==nums[R-1]):
-                        R -= 1
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
                         
-                    L += 1
-                    R -= 1
-                elif nums[i] + nums[L] + nums[R] > 0:
-                    R -= 1
+                    left += 1
+                    right -= 1
+                elif nums[i] + nums[left] + nums[right] > 0:
+                    right -= 1
                 else:
-                    L += 1
+                    left += 1
                     
         return res
