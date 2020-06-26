19869bbf7668291251a260ed02e3b1aaae4c121e
Lazysisphus <624837594@qq.com>
2020-06-26 16:33:25 +0800

    Create Offer22链表中倒数第k个结点.py

---
+++ Offer22链表中倒数第k个结点.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getKthFromEnd(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        # 方法1：快慢指针
+        # 时间复杂度O(n)，空间复杂度O(1)
+        p1, p2 = head, head
+        while k-1:
+            p1 = p1.next
+            k -= 1
+        while p1.next:
+            p1 = p1.next
+            p2 = p2.next
+        return p2
