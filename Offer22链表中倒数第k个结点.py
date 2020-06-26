09c9c5eea99839190a0ef489a8398d7a8e283289
Lazysisphus <624837594@qq.com>
2020-06-26 16:44:06 +0800

    Update Offer22链表中倒数第k个结点.py

--- Offer22链表中倒数第k个结点.py
+++ Offer22链表中倒数第k个结点.py
@@ -13,6 +13,10 @@ class Solution(object):
         """
         # 方法1：快慢指针
         # 时间复杂度O(n)，空间复杂度O(1)
+        # 注意三个地方：
+        #   链表的头结点为空；
+        #   链表的长度小于k
+        #   k为0或负值
         p1, p2 = head, head
         while k-1:
             p1 = p1.next
