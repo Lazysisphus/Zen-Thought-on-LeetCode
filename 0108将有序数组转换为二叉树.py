2da4d75d9c0ed23ebd6ee8abd5dd72540ec583a2
Lazysisphus <33621176+Lazysisphus@users.noreply.github.com>
2020-07-03 11:29:50 +0800

    Create 0108将有序数组转换为二叉树.py

---
+++ 0108将有序数组转换为二叉树.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        # 递归
+        # 时间复杂度O(n)，空间复杂度O(logn)
+        if not nums:
+            return None
+
+        def helper(left, right):
+            if left > right:
+                return None
+            
+            mid = left + (right - left) // 2
+            root = TreeNode(nums[mid])
+            root.left = helper(left, mid-1)
+            root.right = helper(mid+1, right)
+            return root
+
+        return helper(0, len(nums)-1)
