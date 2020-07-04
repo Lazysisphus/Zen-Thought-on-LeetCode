efe995fa9ec87891a55e72a6d51b863e7afcece5
Lazysisphus <624837594@qq.com>
2020-07-05 01:20:42 +0800

    Create Offer40最小的k个数.py

---
+++ Offer40最小的k个数.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def getLeastNumbers(self, arr, k):
+        """
+        :type arr: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        # 方法1：使用快速排序中的partition找到数组第k小的数
+        # 时间复杂度O(n)
+        def partition(arr, low, high):
+            pivot = arr[low]
+            while low < high:
+                while low < high and arr[high] >= pivot:
+                    high -= 1
+                arr[low] = arr[high]
+                while low < high and arr[low] <= pivot:
+                    low += 1
+                arr[high] = arr[low]
+            arr[low] = pivot
+            return low
+
+        if not arr or k <= 0:
+            return [] 
+        low = 0
+        high = len(arr) - 1
+        index = partition(arr, low, high)
+        while index != k-1:
+            if index > k-1:
+                high = index - 1
+                index = partition(arr, low, high)
+            if index < k-1:
+                low = index + 1
+                index = partition(arr, low, high)
+        return arr[ :k]
