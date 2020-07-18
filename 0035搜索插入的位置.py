3cab37a28bf60d3e537e087270ac618c8accee42
Lazysisphus <33621176+Lazysisphus@users.noreply.github.com>
2020-07-19 00:03:46 +0800

    update

---
+++ 0035搜索插入的位置.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # 有序数组，二分查找
+        # 时间复杂度O(n)，空间复杂度O(1)
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+
+        if nums[left] < target:
+            return left + 1
+        else:
+            return left
