0e44018ef7878c336902e871423aa0b15836d164
Lazysisphus <624837594@qq.com>
2020-08-09 14:52:00 +0800

    Update 0015三数之和.py

--- 数组/0015三数之和.py
+++ 数组/0015三数之和.py
@@ -5,38 +5,36 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         # 排序+双指针遍历
-        # https://leetcode-cn.com/problems/3sum/solution/pai-xu-shuang-zhi-zhen-zhu-xing-jie-shi-python3-by/
+        # 遍历整个数组，在每次遍历中：
+        #   如果当前数字大于0，那么后续无需遍历，直接返回res
+        #   如果当前数字和前一个数字相同，为了去重，直接进行下次遍历
+        #   否则，取当前数字后面的一个数字作为left，取最右侧数字作为right
+        #       当左右两个指针没有相遇，判断i、left、right三个指针指向数字之和与0的关系，同时在此过程中去重
+        # 时间复杂度O(n^2)，空间复杂度O(1)
         n = len(nums)
-        res=[]
         if not nums or n < 3:
-            return res
-
+            return []
+        
+        res = []
         nums.sort()
-        for i in range(n-2):
-            # 如果当前数字已经大于0，那么后续数字没有必要遍历
+        for i in range(n):
             if nums[i] > 0:
                 return res
-            # 避免重复
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-
             left = i + 1
             right = n - 1
             while left < right:
                 if nums[i] + nums[left] + nums[right] == 0:
                     res.append([nums[i], nums[left], nums[right]])
-
-                    # 避免重复
-                    while left < right and nums[left] == nums[left+1]:
+                    while left < right and nums[left] == nums[left + 1]:
                         left += 1
-                    while left < right and nums[right] == nums[right-1]:
+                    while left < right and nums[right] == nums[right - 1]:
                         right -= 1
-                        
                     left += 1
                     right -= 1
                 elif nums[i] + nums[left] + nums[right] > 0:
                     right -= 1
                 else:
                     left += 1
-                    
         return res
