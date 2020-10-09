409360a10575af37496c9ad2580051434c8d55b6
Lazysisphus <624837594@qq.com>
2020-10-09 11:25:15 +0800

    Update 0141环形链表Ⅰ.py

--- 链表/0141环形链表Ⅰ.py
+++ 链表/0141环形链表Ⅰ.py
@@ -1,3 +1,23 @@
+'''
+给定一个链表，判断链表中是否有环。
+
+如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+
+如果链表中存在环，则返回 true 。 否则，返回 false 。
+
+ 
+
+进阶：
+
+你能用 O(1)（即，常量）内存解决此问题吗？
+
+提示：
+
+链表中节点的数目范围是 [0, 104]
+-105 <= Node.val <= 105
+pos 为 -1 或者链表中的一个 有效索引 。
+'''
+
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
@@ -12,12 +32,12 @@ class Solution(object):
         """
         # 使用快慢指针
         # 时间复杂度O(n)
-        if not head or nor head.next:
+        if not head:
             return False
         
         flag = False
         fast = slow = head
-        while fast.next and fast.next.next:
+        while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
             if fast == slow:
